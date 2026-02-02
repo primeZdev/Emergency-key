edf2946b3e365a99c623817b372a41a67098ebb0
primeZdev <the.primez.dev@gmail.com>
2026-02-02 13:35:13 +0330

    feat(API)

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/primeZdev/Emergency-key/internal/config"
+)
+
+func main() {
+	app := fiber.New()
+
+	config := config.LoadConfig()
+	keys, err := os.ReadFile("keys.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Get("/"+config.APIKey, func(c *fiber.Ctx) error {
+		return c.SendString(string(keys))
+
+	})
+
+	app.Listen(":" + config.Port)
+}
