0f9be55401561445fa3cb89f06b8b6a1b1dc445e
primeZdev <the.primez.dev@gmail.com>
2026-02-02 13:17:45 +0330

    feat: added load values fron .env

---
+++ internal/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port   string
+	APIKey string
+}
+
+// LoadConfig loads all values from .env
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	apiKey := os.Getenv("API_KEY")
+
+	return &Config{
+		Port:   port,
+		APIKey: apiKey,
+	}
+}
